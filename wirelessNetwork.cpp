2cd1c978073620006b87c0d77f7508e0b0d54ddb
Albertorio <albertorio.10@hotmail.com>
2014-03-10 15:33:12 -0400

    Update wirelessNetwork.cpp

--- wirelessNetwork.cpp
+++ wirelessNetwork.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 wirelessNetwork::wirelessNetwork(){
 
-    wirelessNetwork(10.0, 500);//contructor default crea un wirelessNetwork 10x10 con 500 nodos
+    wirelessNetwork(10.0, 500);//Default constructor: creates a 10x10 wirelessNetwork with 500 nodes
 
 }
 
@@ -18,13 +18,14 @@ wirelessNetwork::wirelessNetwork(float size, int n){
     G.sizeGraph = size;
     for(int i=0;i<n;i++){
         ID.push_back(i);
-        posX.push_back(((double)rand()/(RAND_MAX))*size);   //crea una coordenada decimal random
+        //creates a random coordinate
+        posX.push_back(((double)rand()/(RAND_MAX))*size);  
         posY.push_back(((double)rand()/(RAND_MAX))*size);
-        G.addVertex(ID[i],posX[i],posY[i]); // inserta esas coordenadas en su respectivo vertice
+        G.addVertex(ID[i],posX[i],posY[i]); //Inserts coordinates to node
     }
-
-    //Aqui se calcula la distancia entre vertices
-    //si es de 1 o menos esos vertices van conectados con una arista
+	
+    //Here we calculate distance between nodes
+    //if the distance is 1 or less those nodes will be connected by an edge
     for(int i=0;i<n;i++){
         for(int j=i+1;j<n;j++){
             float dist = peso(i,j);
@@ -35,44 +36,44 @@ wirelessNetwork::wirelessNetwork(float size, int n){
     }
 }
 
-//funcion: calcula distancia entre vertices
+//function:calculate distance between nodes
 float wirelessNetwork::peso(int i, int j){
-    //formula de distancia entre dos puntos
+    //Distance formula for two points	
     float dist = sqrt(pow(G.Vertices[i].x - G.Vertices[j].x,2) + pow(G.Vertices[i].y-G.Vertices[j].y,2));
 
     return dist;
 }
 
-//funcion: Disminuye el numero de aristas
+//function: Lowers the number of edges
 void wirelessNetwork::topologyControl(){
-    vector<int>bye; //vector de aristas a borrar
+    vector<int>bye; //Vector of edges to delete
 
     for(int i=0;i<G.Vertices.size();i++){
-           vector<adData>nei;   //vector que va a tener los vecinos del "current node"
-            if(!G.Vertices[i].adjacencyList.empty()){ //verifica que tenga vecinos
+           vector<adData>nei;   //vector that has the neighbors of the "current node"
+            if(!G.Vertices[i].adjacencyList.empty()){ //checks if there are any neighbors
                 nei = G.getNeighbors(i);
             }
         for(int j=0;j<G.Vertices[i].adjacencyList.size();j++){
-            vector<adData>neinei;   //vector que tendra los vecinos del vecino del vertice
+            vector<adData>neinei;   //Vector that has the neighbors of the neighbors of the "current node"
             if(!G.Vertices[nei[j].to].adjacencyList.empty()){
                 neinei=G.getNeighbors(nei[j].to);
             }
             for(int x=0;x<G.Vertices[nei[j].to].adjacencyList.size();x++){
-               if(peso(i,neinei[x].to) != 0 && peso(nei[j].to,neinei[x].to) !=0){//if verifica que no estemos que el vecino no sea el mismo
-               if(peso(i,neinei[x].to) < peso(i,nei[j].to) && peso(nei[j].to,neinei[x].to) < peso(i,nei[j].to)){//comparamos distancia entre nodo y vecino, nodo y vecino del vecino
-                    bye.push_back(nei[j].to); //si la distancia entre el nodo y vecino del vecino es menor que la de nodo a vecino, insertamos el vecino del nodo para borrar
+               if(peso(i,neinei[x].to) != 0 && peso(nei[j].to,neinei[x].to) !=0){//if checks that the node and the neighbor is not the same
+               if(peso(i,neinei[x].to) < peso(i,nei[j].to) && peso(nei[j].to,neinei[x].to) < peso(i,nei[j].to)){//compare distance between node and neighbor, node and neighbor's neighbors.
+                    bye.push_back(nei[j].to); //if the distance between "current node" and neighbor's neighbor is less than "current node" and neighbor we push the "current node" neighbor to the vector to delete.
                     }
                 }
             }
         }
-        for(int y=0;y<bye.size();y++){//borramos los vecinos
+        for(int y=0;y<bye.size();y++){//delete neighbors
             G.deleteEdge(bye[y],i);
         }
-        bye.clear();//limpiamos vector para no tratar de borrar cosas que ya borramos
+        bye.clear();//clean vector so we dont try to delete things that we already erased
     }
 }
 
-//funcion: verifica si el nodo ya ha sido visitado o no
+//function: verifies if the node was visited or not
 bool wirelessNetwork::isVisited(int vertex, vector<int> arr){
 		for (int i = 0; i<arr.size(); i++)
 		{
@@ -82,7 +83,7 @@ bool wirelessNetwork::isVisited(int vertex, vector<int> arr){
 		return false;
 }
 
-//funcion: inserta elemento a la lista de compass routing
+//function: inserts element to the compass routing list
 vector<int> wirelessNetwork::addToList(int head, vector<int> body){
  	int size = body.size();
 		vector<int> merged;
@@ -112,7 +113,7 @@ vector<int> wirelessNetwork::compassRouting(int s, int t)
 		}
 
 
-		else if (s == t) //Verifica si es el mismo nodo
+		else if (s == t) //Verifies if its the same node
 		{
 			ruta.push_back(t);
 			visitado.clear();
@@ -129,14 +130,14 @@ vector<int> wirelessNetwork::compassRouting(int s, int t)
 			int num = neigh.size();
 
 
-			if (num==0) // verifica si no tiene vecinos
+			if (num==0) // verifies if it doesnt have neighbors
 			{
 				vector<int>emptyRoute;
 				visitado.clear();
 				return emptyRoute;
 			}
 
-			else if ((num==1)&&(isVisited(neigh[0].to, visitado)))//verifica si solo tiene un vecino
+			else if ((num==1)&&(isVisited(neigh[0].to, visitado)))//verifies if it only has one neighbor
 			{
 				vector<int>emptyRoute;
 				visitado.clear();
@@ -148,7 +149,7 @@ vector<int> wirelessNetwork::compassRouting(int s, int t)
 				double maxCosine = -2;
 				int next = s;
 
-				//Encuentra vecino con angulo minimo
+				//Finds neighbor with minimum angle
 				for (int i = 0; i<num; i++)
 				{
 					int neighbor = neigh[i].to;
@@ -158,8 +159,8 @@ vector<int> wirelessNetwork::compassRouting(int s, int t)
 						next = neighbor;
 					}
 				}
-
-				//escoge al proximo vecino para seguir el comppass routing
+				
+				//Selects the next neighbor to continue the compass routing
 				visitado.push_back(s);
 				return addToList(s, compassRouting(next, t));
 			}
